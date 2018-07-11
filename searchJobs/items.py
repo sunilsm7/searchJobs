d2f27d91ba8760da50828f253ca90e2d4a2f9d28
Sunil <sunil9766@gmail.com>
2018-07-11 13:50:22 +0530

    initial commit

---
+++ searchJobs/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ShellJobItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    jobdetail_link = scrapy.Field()
+    job_description = scrapy.Field()
+    skill_group = scrapy.Field()
+    location = scrapy.Field()
+    last_updated = scrapy.Field()
+    hot_job = scrapy.Field()
